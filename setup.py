f8115902a563c5ac98493782c477c4481dab3f69
sappy Chi <sappy5678@gmail.com>
2020-02-10 18:56:22 +0800

    first build for pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+PYPI_NAME = "NTHU_NLPLab_toolkit"
+
+with open(f'NTHU_NLPLab_toolkit/requirements.txt') as fp:
+    install_requires = fp.readlines()
+
+install_requires = filter(lambda x: x.strip() != '', install_requires)
+install_requires = list(install_requires)
+
 setuptools.setup(
-    name="NTHU_NLPLab_toolkit",  # Replace with your own username
-    version="0.0.3",
+    name=f"{PYPI_NAME}",  # Replace with your own username
+    version="0.0.1",
     author="sappy5678",
     author_email="sappy@nlplab.cc",
     description="This toolkit is maintained by NTHU NLP Lab",
@@ -13,6 +21,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/NTHU-NLPLAB/NLPLab_toolkit",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
