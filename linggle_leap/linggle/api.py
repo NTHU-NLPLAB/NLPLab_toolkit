e5bcbf80911a92a2a51e1a2b76a35d50b8b462df
sappy5678 <sappy5678@gmail.com>
2020-05-02 11:41:35 +0800

    refactoring and fix bugs

--- linggle_leap/linggle/api.py
+++ linggle_leap/linggle/api.py
@@ -5,9 +5,48 @@ from abc import abstractmethod
 from .config import NGRAM_API_URI, EXP_API_URI, API_LIST, NgramResult
 
 
-class Factory:
+class APIFactory:
+    """This is `Linggle <https://linggle.com/>`_ api factory class.
+    you can use ver parameter to select different API version.
+
+    ver: Version can be `www`, `coca`, `cna`, `udn`, `zh, `x`, `bi`
+    
+    Parameters
+    ----------
+    ver : {'www', 'coca', 'cna', 'udn', 'zh', 'x', 'bi'}
+        version of different linggle API
+            * www - Google 1T
+            * coca - Corpus of Contemporary American English
+            * cna - 中央社
+            * udn - 聯合新聞網
+            * zh
+            * x - 雙語Linggle
+            * bi - PhraseBookAPI
+    """
+
     @staticmethod
-    def GetAPI(name="www"):
+    def get_api(name="www"):
+        """This function geterate Linggle api by query argument
+
+        Parameters
+        ----------
+        name : str{'www', 'coca', 'cna', 'udn', 'zh', 'x', 'bi'}
+            The query string to query Linggle
+            you can check `Linggle <https://linggle.com/>`_ for more details.
+     
+        Returns
+        -------
+        NgramResult : dict
+            query, ngrams, total
+
+
+
+        Example
+        -------
+        >>> from linggle_leap.linggle.api import APIFactory
+        >>> api = APIFactory.get_api('www')
+
+        """
         if name not in API_LIST:
             return None
         if name in ["www", "coca", "cna", "udn", "zh", "x"]:
@@ -52,7 +91,7 @@ class LinggleAPI(API):
     you can use ver parameter to select different API version.
 
     ver: Version can be `www`, `coca`, `cna`, `udn`, `zh, `x`
-
+    
     Parameters
     ----------
     ver : {'www', 'coca', 'cna', 'udn', 'zh', 'x'}
@@ -86,27 +125,24 @@ class LinggleAPI(API):
             Default value is "en".
         Returns
         -------
-        NgramResult : tuple
+        NgramResult : dict
             query, ngrams, total
 
 
 
         Example
         -------
-        >>> from linggle_leap.Linggle.api import Factory
-        >>> api = Factory.GetAPI('www')
+        >>> from linggle_leap.linggle.api import APIFactory
+        >>> api = APIFactory.get_api('www')
         >>> api.query('discuss ?about the issue')
-        NgramResult(query='discuss ?about the issue', \
-ngrams=[['discuss the issue', 147489], ['discuss about the issue', 98]], total=147587)
+        ...
 
         Example - x.linggle
         -------
-        >>> from linggle_leap.Linggle.api import Factory
-        >>> api = Factory.GetAPI('x')
+        >>> from linggle_leap.linggle.api import APIFactory
+        >>> api = APIFactory.get_api('x')
         >>> api.query(u'吃藥', x_lang='zh')
-        NgramResult(query='吃藥', ngrams=[['to take medicine', 13300], ['take medicine', 27990], \
-['and take medicines', 359], ['take medicines', 10861], ['taking medicine', 26999]], \
-total=79509)
+        ...
         """
         if x_lang == "zh" and self.ver == "x":
             # self.ngram_api = NGRAM_API_URI.replace("query", "equery")
@@ -115,7 +151,7 @@ total=79509)
         query = urllib.parse.quote(query, safe="")
         req = requests.get(self.ngram_api + query)
         if req.status_code == 200:
-            return NgramResult(**req.json())
+            return req.json()
 
     def get_example(self, ngram_str):
         """This function query Linggle by query argument
@@ -132,28 +168,11 @@ total=79509)
 
         Example
         -------
-        >>> from linggle_leap.Linggle.api import LinggleAPI
+        >>> from linggle_leap.linggle.api import APIFactory
         >>> from pprint import pprint
-        >>> api = LinggleAPI()
+        >>> api = APIFactory.get_api('www')
         >>> pprint(api.get_example('get a'))
-        ['An aid organization helps the family get an apartment in Long Beach , '
-         'California , and helps Riku get a job .',
-         'Every evening every citizen could get a copy of every piece of paper '
-         'generated that day in every government office .',
-         'Rookie Bernard Williams figures to start at LT and RT Antone Davis might get '
-         'a look at G. Key concern : QB Randall Cunningham ( broken leg ) returns .',
-         'A Corvette-style rear suspension greatly improved handling , and you could '
-         "even get a @@ 1970 Chevrolet Camaro : Also from GM 's talented Mitchell , "
-         "this Camaro 's stunning body admittedly was inspired by Ferrari 's classic "
-         "1960s 250GT short-wheelbase model , styled by Italy 's inestimable "
-         'Pininfarina .',
-         'I get a hundred letters a month -- from law firms , investigators , people '
-         'who want to come in and evaluate us -- all trying to get us to buy their '
-         'corporate governance services , " said Margaret M. " Peggy " Foran , head of '
-         'corporate governance policy at New York-based drug giant Pfizer Inc . "',
-         'These days , helicopter tours , snorkeling boat rides and hunting safaris '
-         'allow dozens of tourists to get a good view of -- or even set foot on -- '
-         "Hawaii 's seventh-largest island nearly every day ."]
+        [...]
 
         """
         if self.ver == "x":
