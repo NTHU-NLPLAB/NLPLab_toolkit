3ef6437a9763546d13431fcc7557f0edb10d1e94
JJC <jjc@nlplab.cc>
2020-06-12 18:19:10 +0800

    rename!: rename package name and version `0.0.3`
    rename `linggle_leap` to `linggle`
    change `setup.py` and `python_package.yml`
    push version forward

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ install_requires = filter(lambda x: x.strip() != "", install_requires)
 install_requires = list(install_requires)
 
 setuptools.setup(
-    name=PYPI_NAME,  # Replace with your own username
+    name=PYPI_NAME,
     version="0.0.3",
-    author="sappy5678",
-    author_email="sappy@nlplab.cc",
-    description="This toolkit is maintained by NTHU NLP Lab",
+    author="sappy5678, dspp779, ",
+    author_email="sappy@nlplab.cc, jjc@nlplab.cc, isaackch@nlplab.cc",
+    description="This toolkit contains APIs for Linggle, LinggleGEC, LinggleWrite, and LinggleBooster.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/NTHU-NLPLAB/NLPLab_toolkit",
