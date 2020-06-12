ceeae658bea8ad61d1e152f749d2480e3b76c9b6
JJC <jjc@nlplab.cc>
2020-06-12 17:17:16 +0800

    refactor!: move `linggle.linggle` to `linggle`

--- linggle/__init__.py
+++ linggle/__init__.py
@@ -1,4 +1,4 @@
-from leap.linggle.linggle_api import Linggle
-from leap.linggle.tpb_api import TotalPhraseBook
+from .linggle_api import Linggle
+from .tpb_api import TotalPhraseBook
 
 __all__ = ["Linggle", "TotalPhraseBook"]
